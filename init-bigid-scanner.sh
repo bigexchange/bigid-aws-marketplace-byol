d22c6e921fd8b850e9f0b3a6369b6444a34f7440
shayb <shayb@bigid.com>
2019-11-18 15:54:08 +0200

    create rmq certs on execution

--- init-bigid-scanner.sh
+++ init-bigid-scanner.sh
@@ -3,7 +3,6 @@ echo "Initializing BigID scanner volume"
 set -e
 docker volume create --name bigid-scanner-data
 docker run -d -v bigid-scanner-data:/etc/scanner --name bigid-scanner 742572016373.dkr.ecr.us-east-1.amazonaws.com/bigid-scanner:${BIGID_VERSION}
-#docker run -d -v bigid-scanner-data:/etc/scanner --name bigid-scanner bigid/bigid-scanner:release
 sleep 10
 docker cp ./kerberos/krb5.conf bigid-scanner:/etc/scanner/krb5.conf
 docker cp ./kerberos/hdfs.keytab bigid-scanner:/etc/scanner/hdfs.keytab
