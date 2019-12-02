bf92b0d1dfa0362ea69e180eac1c91d967c2392f
shayb <shayb@bigid.com>
2019-12-02 20:03:05 +0200

    fix typo

--- init-bigid-scanner.sh
+++ init-bigid-scanner.sh
@@ -2,7 +2,7 @@
 echo "Initializing BigID scanner volume"
 set -e
 docker volume create --name bigid-scanner-data
-docker run -d -v bigid-scanner-data:/etc/scanner --name bigid-scanner 403495124976.dkr.ecr.us-east-1.amazonaws.com/2ba1356e-b944-40e3-a585-25623721f6fd/cg-2902229646/bigid-scanner::${BIGID_VERSION}
+docker run -d -v bigid-scanner-data:/etc/scanner --name bigid-scanner 403495124976.dkr.ecr.us-east-1.amazonaws.com/2ba1356e-b944-40e3-a585-25623721f6fd/cg-2902229646/bigid-scanner:${BIGID_VERSION}
 sleep 10
 docker exec -i bigid-scanner sudo chown -R bigid:bigid /etc/scanner
 docker stop bigid-scanner
